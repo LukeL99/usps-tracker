44e4ac7054f2b5475ae019a5019181e16fc08029
Luke Libraro <lukel99@gmail.com>
2013-07-17 15:33:32 -0500

    added clear map function.

--- js/tracker.js
+++ js/tracker.js
@@ -1,5 +1,6 @@
 var map;
 var sender;
+var markersToDelete = [];
 
 $(document).ready(function(){
 	var mapOptions = {
@@ -15,7 +16,7 @@ $(document).ready(function(){
 
 		sender.latLng = new google.maps.LatLng(parseFloat(sender['latitude']), parseFloat(sender['longitude']));
 
-		build_recipient_list(recipients);
+		buildRecipientList(recipients);
 
 		var sender_icon = sender['profile_pic'];
 
@@ -28,7 +29,7 @@ $(document).ready(function(){
 	});
 });
 
-function animate_delivery(event) {
+function animateDelivery(event) {
 	// ANIMATION CONSTANTS
 	const numSegs = 50;
 	const millisPerSeg = 10;
@@ -61,19 +62,23 @@ function animate_delivery(event) {
 	var lngDist = (stopArray[legCount + 1].lng() - stopArray[legCount].lng()) / numSegs;
 
 	function drawLine() {
+		if(segCount == 0 && legCount == 0){
+			clearMap();
+		}
+		
 		// if we've reached the next leg of the trip
 		if (segCount == numSegs) {
 			incrementLeg();
 		} else {
 			// calculate end point
 			var endPoint = new google.maps.LatLng(stopArray[legCount].lat() + (latDist * (segCount + 1)), stopArray[legCount].lng() + (lngDist * (segCount + 1)));
-			new google.maps.Polyline({
+			markersToDelete.push(new google.maps.Polyline({
 				map : map,
 				path : [segStartPoint, endPoint],
 				strokeColor : '#FF0000',
 				strokeOpacity : 1.0,
 				strokeWeight : 2
-			});
+			}));
 			segStartPoint = endPoint;
 			segCount++;
 		}
@@ -87,11 +92,11 @@ function animate_delivery(event) {
 			latDist = (stopArray[legCount + 1].lat() - stopArray[legCount].lat()) / numSegs;
 			lngDist = (stopArray[legCount + 1].lng() - stopArray[legCount].lng()) / numSegs;
 
-			var pinMarker = new google.maps.Marker({
+			markersToDelete.push(new google.maps.Marker({
 				position : stopArray[legCount],
 				map : map,
 				animation : google.maps.Animation.DROP
-			});
+			}));
 			segCount = 0;
 		} else {
 			endAnimation();
@@ -100,27 +105,34 @@ function animate_delivery(event) {
 
 	function endAnimation() {
 		clearInterval(lineInterval);
-		var recipientMarker = new google.maps.Marker({
+		markersToDelete.push(new google.maps.Marker({
 			position : stopArray[legCount],
 			map : map,
 			icon : recipientIcon,
 			animation : google.maps.Animation.DROP
-		});
+		}));
 	}
 
 }
 
-function build_recipient_list(recipients) {
-	$.each(recipients, function(key, val) {
-		var recipient_div = $('#recipient-template').clone();
+function clearMap(){
+	$.each(markersToDelete, function(key, val){
+		val.setMap(null);
+	});
+};
 
-		recipient_div.find('.recipient-icon-img').prop('src', val['profile_pic']);
-		recipient_div.find('.recipient-name').text(val['name']);
-		recipient_div.find('.recipient-city').text(val['city']);
-		recipient_div.find('.recipient-delivery-status').text(val['status']);
-		recipient_div.appendTo('#tracker-menu').show();
-		recipient_div.bind('click', val, animate_delivery);
+function buildRecipientList(recipients) {
+	var i = 1;
+	$.each(recipients, function(key, val) {
+		var recipientImg = $('.fPanel' + i.toString());
+		// recipient_div.find('.recipient-icon-img').prop('src', val['profile_pic']);
+		// recipient_div.find('.recipient-name').text(val['name']);
+		// recipient_div.find('.recipient-city').text(val['city']);
+		// recipient_div.find('.recipient-delivery-status').text(val['status']);
+		// recipient_div.appendTo('#tracker-menu').show();
+		recipientImg.bind('click', val, animateDelivery);
+		i++;
 	});
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+// google.maps.event.addDomListener(window, 'load', initialize);
